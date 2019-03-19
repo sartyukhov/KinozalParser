ce18eb64dc9b1b717f36dc6c04950e3e27951d2b
sartyukhov <artyukhov.svyatoslav@gmail.com>
2019-03-19 17:19:02 +0300

    v 0.1.0
    - page parsing done
    - repeats deleted
    - simple config done

---
+++ pageParser.py
@@ -0,0 +1,106 @@
+from urllib.request import *
+from re             import *
+
+
+#select quality
+class Quality():
+    _4K    = '4K'
+    _1080p = '1080p'
+    _720p  = '720p'
+    url = {
+        _4K    : 7,
+        _1080p : 3,
+        _720p  : 3
+    }
+
+#select quality
+class Source():
+    BDRip  = 'BDRip'
+    Web_DL = 'WEB-DL'
+
+#select sort
+class Sort():
+    Sids = 1
+    Pirs = 2
+
+#select days
+class Days():
+    _1 = 1
+    _3 = 3
+
+# select script working mode
+class Mode:
+    LOCAL = 1
+    WEB   = 2
+
+QUALITY = Quality._1080p
+SOURCE  = Source.BDRip
+SORT    = Sort.Pirs
+DAYS    = Days._3
+#debug - LOCAL / Release - WEB
+MODE    = Mode.LOCAL
+
+class FilesContainer:
+    def __init__(self):
+        self.files = []
+    def __getitem__(self, key):
+        return self.files[key]
+    def __setitem__(self, key, val):
+        self.files[key] = val
+    def __iter__(self):
+        return iter(self.files)
+    def __contains__(self, item):
+        for f in self.files:
+            if f.name == item.name:
+                return True
+        return False
+    def appendUnique(self, val):
+        if val not in self:
+            self.append(val)
+    def append(self, val):
+        self.files.append(val)
+    def sort(self):
+        self.files = sorted(self.files, key=lambda f: f.name)
+    def getList(self):
+        return '\n'.join(x.name for x in self.files)
+
+class Video:
+    def __init__(self, name):
+        self.name = name
+
+def parse():
+    htmlFile = 'page.html'
+    # download and save HTML page
+    if MODE == Mode.WEB:
+        url = "http://kinozal.tv/browse.php?s=&g=0&c=1002&v={q}&d=0&w={d}&t={s}&f=0"\
+            .format(q=Quality.url[QUALITY], d=DAYS, s=SORT)
+        with urlopen(url) as page:
+            with open(htmlFile, 'wb') as outputHTML:
+                outputHTML.write(page.read())
+    # get saved HTML data to parse
+    try:
+        with open(htmlFile, 'r', encoding='windows 1251') as inputHTML:
+            pageContent = inputHTML.read()
+    except:
+        with open(htmlFile, 'r', encoding='UTF-8') as inputHTML:
+            pageContent = inputHTML.read()
+    # parsing (yeah, just one string)
+    findPattern = r'"r\d">([^/]+).*{s}.*{q}'.format(s=SOURCE, q=QUALITY)
+    return findall(findPattern, pageContent)
+
+def main():
+    parsed = parse()
+    filesContainer = FilesContainer()
+
+    for name in parsed:
+        filesContainer.appendUnique(Video(name))
+    
+    # sort is unnecessary now
+    # filesContainer.sort()
+
+    with open('page.txt', 'w') as outputTXT:
+        outputTXT.write(filesContainer.getList())
+
+
+if __name__ == "__main__":
+    main()
