4fb3ee2ea3abde76ade238014e4470dba8ee3ee8
sartyukhov <artyukhov.svyatoslav@gmail.com>
2019-03-26 12:29:14 +0300

    v 0.5.5d
    - add a lot

--- newParser.py
+++ newParser.py
@@ -5,11 +5,45 @@ from time           import time, gmtime, strftime
 from enum           import Enum
 from urlOpener      import getUrlContent
 
+#select file
+class Content(Enum):
+    TV_SHOWS = '1001'
+    MOVIES   = '1002'
+    CARTOONS = '1003'
+
+#select quality
+class Quality(Enum):
+    _4K    = '7'
+    _1080P = '3'
+    _720P  = '3'
+
+#select sort
+class Sort(Enum):
+    SIDS = '1'
+    PIRS = '2'
+    SIZE = '3'
+
+#select days
+class Days(Enum):
+    ANY = '0'
+    _1  = '1'
+    _3  = '3'
+
 class TorrentsContainer:
-    def __init__(self, group):
+    baseUrl = 'http://kinozal.tv/browse.php?'
+    def __init__(self, content, sort=Sort.PIRS):
         self.created = time() + 10800 # UTC+3
+        self.content = content
+        self.sort    = sort
         self.files   = []
-        self.group   = group
+        #update container with content
+        url = self.baseUrl + 's=&g=0&c={c}&v=0&d=0&w=0&t={t}&f=0&page=0'.format(
+            c=self.content,
+            t=self.sort
+        )        
+        torrents = parseTorrentsList(getUrlContent(url, name='page'))
+        for t in torrents:
+            self.appendUnique(Torrent(t[0], t[1]))
 
     def __iter__(self):
         return iter(self.files)
@@ -28,17 +62,19 @@ class TorrentsContainer:
             return True
 
     def append(self, item):
-        item.serachSelf(self.group)
+        item.getMoreInfo()
+        item.serachMirrors(self.content)
         self.files.append(item)
 
     def sort(self):
         self.files = sorted(self.files, key=lambda f: f.name)
 
 
-
 class Torrent:
+    baseUrl = 'http://kinozal.tv/details.php?id='
     def __init__(self, id, info):
         self.id = id
+        self.qualitys = []
         self.__parseInfo(info)
         
     def __parseInfo(self, info):
@@ -46,40 +82,68 @@ class Torrent:
         self.name = search(r'[^/]*', info).group()
         self.year = search(r'[0-9]{4}(-[0-9]{4})?', info).group()
 
-    def serachSelf(self, group):
-        surl = 'http://kinozal.tv/browse.php?s={s}&g=0&c={c}&v=0&d={d}&w=0&t=3&f=0'.format(
+    def getMoreInfo(self):
+        turl = self.baseUrl + self.id
+        parsed = parseTorrentPage(getUrlContent(turl, name='tor_page'), rating=True)
+        self.imdbUrl    = parsed.get('raturl', '?')
+        self.imdbRating = parsed.get('rating', '?')
+
+    def serachMirrors(self, content, sort=Sort.SIZE):
+        surl = 'http://kinozal.tv/browse.php?s={s}&g=0&c={c}&v=0&d={d}&w=0&t={t}&f=0'.format(
             s=quote(self.name),
-            c=group,
-            d=self.year
+            c=content,
+            d=self.year,
+            t=sort
         )
-        selfs = parseTorrentsList(getUrlContent(surl))
-        print(selfs)
+        mirrors = parseTorrentsList(getUrlContent(surl, name='mirrors_page'))
+        for m in mirrors:
+            turl = self.baseUrl + m[0]
+            parsed = parseTorrentPage(
+                getUrlContent(turl, name='tor_page'),
+                size=True,
+                kRating=True,
+                qualitys=True
+            )
+            self.qualitys.append(parsed.get('quality', '?'))
+        print(self.qualitys)
 
 
 def parseTorrentsList(content):
     # parsing (yeah, just one string)
-    fp = r'.*class="nam"><a href="/details.php\?id=(\d+).*">(.*)</a>.*>'
+    fp = r'.*class="nam"><a href=".*/details.php\?id=(\d+).*">(.*)</a>.*>'
     return findall(fp, content)
 
-def parseTorrentPage(content):
+def parseTorrentPage(content, rating=False, size=False, kRating=False, qualitys=False):
     d = dict()
-    findResult = findall(r'.*href="(.*)" target=.*>IMDb<span class=.*>(.*)</span>', content)
-    if len(findResult) > 0:
-        d['raturl'] = findResult[0][0]
-        d['rating'] = findResult[0][1]
-    findResult = findall(r'.*>Вес<span class=".*>(.*)\(.*\)</span>', content)
-    if len(findResult) > 0:
-        d['size'] = findResult[0]
+
+    if rating:
+        for db in ('IMDb', 'Кинопоиск'):
+            pattern = r'.*href="(.*)" target=.*>{}<span class=.*>(.*)</span>'.format(db)
+            findResult = findall(pattern, content)
+            if len(findResult) > 0:
+                d['raturl'] = findResult[0][0]
+                d['rating'] = findResult[0][1]
+                break
+
+    if size:
+        findResult = findall(r'.*>Вес<span class=".*>(.*)\(.*\)</span>', content)
+        if len(findResult) > 0:
+            d['size'] = findResult[0]
+
+    if kRating:
+        findResult = findall(r'.*<span itemprop="ratingValue">(.*)</span>', content)
+        if len(findResult) > 0:
+            d['kRating'] = findResult[0]
+
+    if qualitys:
+        findResult = findall(r'.*<b>Качество:</b>(.*)<br>', content)
+        if len(findResult) > 0:
+            d['quality'] = findResult[0]       
+
     return d
 
 def updateDB():
-    url = 'http://kinozal.tv/browse.php?s=&g=0&c=1002&v=0&d=0&w=0&t=2&f=0&page=0'
-
-    moviesContainer = TorrentsContainer('1002')
-
-    torrents = parseTorrentsList(getUrlContent(url))
-    for t in torrents:
-        moviesContainer.appendUnique(Torrent(t[0], t[1]))
+    moviesContainer = TorrentsContainer(Content.MOVIES)
 
     
 
@@ -87,6 +151,7 @@ def updateDB():
 
 
 if __name__ == "__main__":
-    # url = 'http://kinozal.tv/browse.php?s=&g=0&c=1002&v=0&d=0&w=0&t=2&f=0&page=0'
-    # print(parseTorrentsList(getUrlContent(url)))
     updateDB()
+    # c = getUrlContent('surl', name='self_page')
+    # selfs = findall(r'.*href="/details.php\?id=(\d+).*', c)
+    # print(selfs)
